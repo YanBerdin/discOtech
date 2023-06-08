7262cc39f510d3a4d04fd8f61314938719e328d1
evaredon <evaredon2001@gmail.com>
2023-06-08 20:06:59 +0200

    album card v2

--- Front/src/components/AlbumCard/AlbumCard.jsx
+++ Front/src/components/AlbumCard/AlbumCard.jsx
@@ -3,14 +3,10 @@ import './AlbumCard.scss';
 function AlbumCard() {
   return (
     <div className="AlbumCard">
-      <div className="AlbumCard-Picture" />
       <div className="AlbumCard-Description">
         <h3>Album Title</h3>
         <p>Artist Name</p>
       </div>
-      <button type="submit" className="AlbumCard-Button">
-        <div className="AlbumCard-Arrow" />
-      </button>
     </div>
   );
 }
