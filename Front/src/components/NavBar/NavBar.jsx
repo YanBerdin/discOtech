293c005e7ed95570f8830c5cf012d9cee08837a1
sahramarchand <sahra.brooks.sb@gmail.com>
2023-06-10 00:34:38 +0200

    fix navbar for mobile add dropdown

--- Front/src/components/NavBar/NavBar.jsx
+++ Front/src/components/NavBar/NavBar.jsx
@@ -1,141 +1,89 @@
-import './NavBar.scss';
+import { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import { useState, useEffect } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { CSSTransition } from 'react-transition-group';
+import './NavBar.scss';
+import logo from '../../assets/Logo-round.png';
+import user from '../../assets/user-icon.svg';
 import home from '../../assets/home-icon.svg';
 import style from '../../assets/style-icon.svg';
 import heart from '../../assets/heart-icon.svg';
 import about from '../../assets/about-icon.svg';
-import user from '../../assets/user-icon.svg';
-import logo from '../../assets/Logo.png';
 
 function NavBar() {
-  const [activeLink, setActiveLink] = useState('accueil');
-  const [scrolled, setScrolled] = useState(false);
-  const [navExpanded, setNavExpanded] = useState(false);
+  const [toggleOpen, setToggleOpen] = useState(false);
 
-  useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    window.addEventListener('scroll', onScroll);
-
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
-
-  const onUpdateActiveLink = (value) => {
-    setActiveLink(value);
-  };
-
-  const handleNavToggle = () => {
-    setNavExpanded(!navExpanded);
+  const handleToggle = () => {
+    setToggleOpen(!toggleOpen);
   };
 
   return (
-    <Navbar
-      expand="md"
-      className={`${scrolled ? 'scrolled' : ''} ${navExpanded ? 'nav-expanded' : ''}`}
-      onToggle={handleNavToggle}
-    >
-      <Container className="navbar-container">
-        <Navbar.Brand href="/">
-          <img src={logo} alt="Logo" />
+    <Navbar collapseOnSelect expand="lg">
+      <Container>
+        <Navbar.Brand className={`logo-img ${toggleOpen ? 'hidden' : ''}`} href="#accueil">
+          <img
+            src={logo}
+            width="60"
+            height="60"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon" />
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link
-              href="#Accueil"
-              className={
-                activeLink === 'Accueil' ? 'active navbar-link' : 'navbar-link'
-              }
-              onClick={() => onUpdateActiveLink('Accueil')}
-            >
-              <img className="nav-icon" src={home} alt="Icone de l'accueil" />
+
+        <Navbar.Brand className={`user-img ${toggleOpen ? 'hidden' : ''}`} href="#connexion">
+          <img
+            src={user}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Icone pour la page de connexion"
+          />
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={handleToggle} />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+
+            <Nav.Link href="#accueil">
+              <img src={home} alt="Icone de l'accueil" className="nav-icon" />
               Accueil
             </Nav.Link>
-            <Nav.Link
-              href="#Styles"
-              className={
-                activeLink === 'Styles' ? 'active navbar-link' : 'navbar-link'
-              }
-              onClick={() => onUpdateActiveLink('Styles')}
-            >
-              <img className="nav-icon" src={style} alt="Icone de la page style" />
+
+            <Nav.Link href="#styles">
+              <img src={style} alt="Icone de la page des styles" className="nav-icon" />
               Styles
             </Nav.Link>
-            <Nav.Link
-              href="#Favoris"
-              className={
-                activeLink === 'Favoris' ? 'active navbar-link' : 'navbar-link'
-              }
-              onClick={() => onUpdateActiveLink('Favoris')}
-            >
-              <img className="nav-icon" src={heart} alt="Icone de la page favoris" />
+          </Nav>
+          <Nav>
+
+            <Nav.Link href="#favoris">
+              <img src={heart} alt="Icone de la page des favoris" className="nav-icon" />
               Favoris
             </Nav.Link>
-            <Nav.Link
-              href="#A-Propos"
-              className={
-                activeLink === 'A.Propos' ? 'active navbar-link' : 'navbar-link'
-              }
-              onClick={() => onUpdateActiveLink('A.Propos')}
-            >
-              <img className="nav-icon" src={about} alt="Icone de la page à propos" />
-              A Propos
+
+            <Nav.Link eventKey={2} href="#aPropos">
+              <img src={about} alt="Icone de la page des favoris" className="nav-icon" />
+              À propos
             </Nav.Link>
-            <Nav.Link
-              href="#Utilisateur"
-              className={
-                activeLink === 'Utilisateur' ? 'active navbar-link user-link' : 'navbar-link user-link'
-              }
-              onClick={() => onUpdateActiveLink('Utilisateur')}
-            >
-              <img className="user-img" src={user} alt="Icone de la page à propos" />
-            </Nav.Link>
+            <NavDropdown title="Inscription/Connexion" id="collasible-nav-dropdown">
+
+              <NavDropdown.Item href="#connexion">Se connecter</NavDropdown.Item>
+              <NavDropdown.Item href="#inscription">
+                S'inscrire
+              </NavDropdown.Item>
+
+              {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                 <NavDropdown.Item href="#action/3.4">
+                 Separated link
+                </NavDropdown.Item> */}
+            </NavDropdown>
 
           </Nav>
-          <span className="navbar-text">
-            <a to="#connect">
-              <button type="button" className="vvd">
-                S'inscrire
-                <div className="icon">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
-                  >
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path
-                      fill="currentColor"
-                      d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                    />
-                  </svg>
-                </div>
-              </button>
-            </a>
-          </span>
         </Navbar.Collapse>
       </Container>
-      <CSSTransition
-        in={!navExpanded}
-        timeout={200}
-        classNames="user-icon-animation"
-        unmountOnExit
-      >
-        <Nav.Link className="user-logo" href="/">
-          <img src={user} alt="Logo pour la connexion de l'utilisateur" />
-        </Nav.Link>
-      </CSSTransition>
     </Navbar>
   );
 }
