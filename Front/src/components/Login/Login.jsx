b293e5d978969e4351a787a71b30d6d7e7206195
evaredon <evaredon2001@gmail.com>
2023-06-09 12:43:21 +0200

    Login Page design mobile ok

--- Front/src/components/Login/Login.jsx
+++ Front/src/components/Login/Login.jsx
@@ -4,21 +4,29 @@ import User from '../../assets/User.png';
 function Login() {
   return (
     <form className="Login-Form" action="">
-      <img className="Login-UserImg" src={User} alt="Logo de personnage" />
-      <p className="Login-Header">Connexion</p>
-      <div className="Login-InputContainer">
-        <input placeholder="Adresse e-mail" id="email" className="Login-InputField" type="text" />
-      </div>
+      <div className="Login-Card">
+        {/* Login Header : Title and Image */}
+        <div className="Login-Header">
+          <img className="Login-UserImg" src={User} alt="Logo de personnage" />
+          <p className="Login-Title">Connexion</p>
+        </div>
 
-      <div className="Login-InputContainer">
-        <input placeholder="Mot de passe" id="password" className="Login-InputField" type="password" />
-      </div>
+        {/* Login Input : Mail Adress */}
+        <div className="Login-InputContainer">
+          <input placeholder="Adresse e-mail" id="email" className="Login-InputField" type="text" />
+        </div>
 
+        {/* Login Input : Password */}
+        <div className="Login-InputContainer">
+          <input placeholder="Mot de passe" id="password" className="Login-InputField" type="password" />
+        </div>
+
+        {/* Link to SignUp Form */}
+        <p className="Login-SignUp">Pas encore inscrit ? Rejoins nous <a href="#">ici</a> </p>
+      </div>
+      {/* Login Button */}
       <button className="Login-Button" id="button" type="submit">Me connecter</button>
-      <div className="Login-SignUp">
-        <p>Pas encore inscrit ? Rejoins nous <a href="#">ici</a> </p>
 
-      </div>
     </form>
   );
 }
