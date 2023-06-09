267ddd67993c53961d2f6b7bfe33b74d2e88f24d
YanBerdin <yandevformation@gmail.com>
2023-06-09 22:36:05 +0200

    MediaQuery and logos social-media to define

--- Front/src/components/Footer/Footer.jsx
+++ Front/src/components/Footer/Footer.jsx
@@ -1,15 +1,37 @@
 // import React from "react";
 import { NavLink } from 'react-bootstrap';
 import './Footer.scss';
+import Logo from '../../assets/Logo.png';
 // import {BrowserRouteur} from `react-routeur-dom`;
 
 function Footer() {
   return (
-    <footer className="test">
-      <NavLink>A propos</NavLink>
-      <NavLink>Liens utiles</NavLink>
-      <NavLink>Suivez nous</NavLink>
-      <p>Copyright DiscO'Tech - Radium - O'clock - 2023</p>
+    <footer>
+      <section className="footerContainer">
+        <img src={Logo} className="Logo" alt="Logo de cette app" />
+
+        <div className="apropos">
+          <p>A propos</p>
+          <NavLink>L'équipe de developpement</NavLink>
+          <NavLink>Nous contacter</NavLink>
+          <NavLink>Descriptif du projet</NavLink>
+        </div>
+        <div className="liens-utiles">
+          <p>Liens utiles</p>
+          <NavLink>Conditions générales d'utilisation</NavLink>
+          <NavLink>Mentions Légales</NavLink>
+        </div>
+        <div className="reseaux-sociaux">
+          <img
+            className="icon-reseaux"
+            src="https://e7.pngegg.com/pngimages/288/247/png-clipart-youtube-graphy-social-media-computer-icons-facebook-youtube-text-trademark.png"
+            alt="Logo facebook"
+          />
+        </div>
+      </section>
+      <div>
+        <p>Copyright DiscO'Tech - Radium - O'clock - 2023</p>
+      </div>
     </footer>
   );
 }
