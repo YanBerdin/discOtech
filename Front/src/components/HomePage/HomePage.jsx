614a14bc6cd504753eeebd903bb814eded611326
evaredon <evaredon2001@gmail.com>
2023-06-12 23:49:55 +0200

    Prepare home files

---
+++ Front/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+// == Imports : local
+import Header from '../Header/Header';
+import SearchBar from '../SearchBar/SearchBar';
+import Footer from '../Footer/Footer';
+
+// == Component
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <Header />
+      <SearchBar />
+      <Footer />
+    </div>
+
+  );
+}
+export default HomePage;
