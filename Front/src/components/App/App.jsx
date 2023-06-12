44daab3e2d46bda9269cf7195e6d2f4d3819fded
evaredon <evaredon2001@gmail.com>
2023-06-12 23:28:18 +0200

    Prepare the Home Page Component

--- Front/src/components/App/App.jsx
+++ Front/src/components/App/App.jsx
@@ -4,12 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 // Fichiers JSX
 import Login from '../Form/Login/Login';
 import SignUp from '../Form/SignUp/SignUp';
+import HomePage from '../HomePage/HomePage';
 
-import SearchBar from '../SearchBar/SearchBar';
 import NavBar from '../NavBar/NavBar';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-
 
 // Fichier Styles
 import './App.scss';
@@ -18,11 +15,8 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Header />
-      <SearchBar />
-
       <Routes>
-        <Route path="/" element={<div>Je suis rendu quand on est sur /, sinon non</div>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/connexion" element={<Login />} />
         <Route path="/inscription" element={<SignUp />} />
         <Route path="/styles" />
@@ -34,7 +28,8 @@ function App() {
         <Route path="/le-projet" />
         <Route path="/*" />
       </Routes>
-      <Footer />
+
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ Front/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import Header from '../Header/Header';
+import SearchBar from '../SearchBar/SearchBar';
+// import Footer from '../Footer/Footer';
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <SearchBar />
+    </>
+  );
+}
+export default HomePage;
