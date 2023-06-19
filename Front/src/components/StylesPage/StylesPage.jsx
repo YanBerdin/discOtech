96df43d9a43e81863b45d876271136141cb786e4
sahramarchand <sahra.brooks.sb@gmail.com>
2023-06-19 12:25:48 +0200

    add style for mobile

--- Front/src/components/StylesPage/StylesPage.jsx
+++ Front/src/components/StylesPage/StylesPage.jsx
@@ -1,16 +1,23 @@
 import './StylesPage.scss';
 
+import SearchBar from '../SearchBar/SearchBar';
+
 function StylesPage({ styles }) {
   return (
-    <div className="StylesPage">
-      {styles.map((style) => (
-        <div className="StylesPage-Container">
-          <div className="StylesPage-Cards" key={style.id} style={{ backgroundImage: `url(${style.image})` }}>
-            <h3 className="StylesPage-StyleName"> {style.name} </h3>
+    <>
+      <div className="SearchBar">
+        <SearchBar />
+      </div>
+      <div className="StylesPage">
+        {styles.map((style) => (
+          <div className="StylesPage-Container">
+            <div className="StylesPage-Cards" key={style.id} style={{ backgroundImage: `url(${style.image})` }}>
+              <h3 className="StylesPage-StyleName"> {style.name} </h3>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
 
