2fcbb66f017f564cca114de9711c1dbbc3c70d99
YanBerdin <yandevformation@gmail.com>
2023-06-21 19:28:52 +0200

    Controlled Input Done

--- Front/src/components/Form/UserProfile/UserProfileForm/UserProfileForm.jsx
+++ Front/src/components/Form/UserProfile/UserProfileForm/UserProfileForm.jsx
@@ -1,62 +1,109 @@
+/* eslint-disable no-console */
+import { useSelector } from 'react-redux';
 // == Import : local
 import User from '../../../../assets/WelcomeUser.png';
 import './UserProfileForm.scss';
 
 // == Component
 function UserProfileForm() {
+  // const dispatch = usedispatch();
+  const { email } = useSelector((state) => state.user);
+  // Champ controllé des inputs
+  const firstnameInputValue = useSelector(
+    (state) => state.user.firstnameInputValue,
+  );
+  const lastnameInputValue = useSelector(
+    (state) => state.user.lastnameInputValue,
+  );
+  const emailInputValue = useSelector((state) => state.user.emailInputValue);
+  const passwordInputValue = useSelector(
+    (state) => state.user.passwordInputValue,
+  );
+  console.log({ firstnameInputValue });
+  console.log({ lastnameInputValue });
+  console.log({ emailInputValue });
+  console.log({ passwordInputValue });
+  console.log({ email });
   return (
-    <form className="Login-Form" action="">
+    <section className="Login-Form" action="">
       <div className="Login-Card">
         {/* Login Header : Title and Image */}
         <div className="Header-Container">
           <img className="Header-UserImg" src={User} alt="Logo de personnage" />
           <p className="Header-Title">Modifier mon profil</p>
         </div>
-
         {/* Login Input : Nom */}
-        <div className="Field">
+        <form className="Field">
           <input
             required
             className="Field-Input"
+            type="text"
             name="lastname"
             placeholder="Nom"
+            value={lastnameInputValue}
+            onChange={(event) => {
+              console.log(event.target.value);
+            }}
           />
-          <button className="Field-Button" type="submit">&#x1F589;</button>
-        </div>
+          <button className="Field-Button" type="submit">
+            &#x1F589;
+          </button>
+        </form>
         {/* Login Input : Prénom */}
-        <div className="Field">
+        <form className="Field">
           <input
             required
             className="Field-Input"
             name="firstname"
+            type="text"
             placeholder="Prénom"
+            value={firstnameInputValue}
+            onChange={(event) => {
+              console.log(event.target.value);
+            }}
           />
-          <button className="Field-Button" type="submit">&#x1F589;</button>
-        </div>
+          <button className="Field-Button" type="submit">
+            &#x1F589;
+          </button>
+        </form>
 
         {/* Login Input : Mail Adress */}
-        <div className="Field">
+        <form className="Field">
           <input
             required
             className="Field-Input"
             name="email"
+            type="email"
             placeholder="Adresse e-mail"
+            value={emailInputValue}
+            onChange={(event) => {
+              console.log(event.target.value);
+            }}
           />
-          <button className="Field-Button" type="submit">&#x1F589;</button>
-        </div>
+          <button className="Field-Button" type="submit">
+            &#x1F589;
+          </button>
+        </form>
 
         {/* Login Input : Password Adress */}
-        <div className="Field">
+        <form className="Field">
           <input
             required
             className="Field-Input"
             name="password"
+            type="text"
             placeholder="Mot de passe"
+            value={passwordInputValue}
+            onChange={(event) => {
+              console.log(event.target.value);
+            }}
           />
-          <button className="Field-Button" type="submit">&#x1F589;</button>
-        </div>
+          <button className="Field-Button" type="submit">
+            &#x1F589;
+          </button>
+        </form>
       </div>
-    </form>
+    </section>
   );
 }
 
