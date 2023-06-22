eeeda5b3cd6b666885c98c33ed9e2338d209e0fe
evaredon <evaredon2001@gmail.com>
2023-06-22 13:44:46 +0200

    Fix footer and Search Result Design and functionnality ok

--- Front/src/components/SearchBar/SearchBar.jsx
+++ Front/src/components/SearchBar/SearchBar.jsx
@@ -3,66 +3,43 @@
 /* eslint-disable no-console */
 import './SearchBar.scss';
 
-import { useState, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import api from '../../api/api';
-import SearchResult from './SearchResult/SearchResult';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchBar() {
   const [search, setSearch] = useState('');
-  const [albums, setAlbums] = useState([]);
   const navigate = useNavigate();
-  const location = useLocation();
 
   const getAlbums = () => {
-    api
-      .post('/albums/search', { search: search })
-      .then((res) => {
-        setAlbums(res.data);
-        navigate('/resultat-recherche');
-      })
-      .catch((err) => {
-        console.log("Erreur, l'API ne fonctionne plus. Rechargez plus tard.");
-        console.error(err);
-      });
+    navigate(`/resultat-recherche/${search}`);
   };
 
-  useEffect(() => {
-    if (location.pathname !== '/resultat-recherche') {
-      setSearch('');
-      setAlbums([]);
-    }
-  }, [location]);
-
   return (
-    <>
-      <div className="SearchBar">
-        <form
-          className="SearchBar-Form"
-          onSubmit={(event) => {
-            console.log('Envoi de la recherche...');
-            event.preventDefault();
-            getAlbums();
+    <div className="SearchBar">
+      <form
+        className="SearchBar-Form"
+        onSubmit={(event) => {
+          console.log('Envoi de la recherche...');
+          event.preventDefault();
+          getAlbums();
+        }}
+      >
+        <input
+          className="SearchBar-Input"
+          type="search"
+          placeholder="Votre recherche..."
+          aria-label="Search"
+          value={search}
+          onChange={(event) => {
+            setSearch(event.target.value);
+            console.log(event.target.value);
           }}
-        >
-          <input
-            className="SearchBar-Input"
-            type="search"
-            placeholder="Votre recherche..."
-            aria-label="Search"
-            value={search}
-            onChange={(event) => {
-              setSearch(event.target.value);
-              console.log(event.target.value);
-            }}
-          />
-          <button className="SearchBar-Button" type="submit">
-            Rechercher
-          </button>
-        </form>
-      </div>
-      <SearchResult albums={albums} />
-    </>
+        />
+        <button className="SearchBar-Button" type="submit">
+          Rechercher
+        </button>
+      </form>
+    </div>
   );
 }
 
--- Front/src/components/SearchBar/SearchResult/SearchResult.jsx
+++ Front/src/components/SearchBar/SearchResult/SearchResult.jsx
@@ -1,11 +1,31 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import AlbumCard from '../../HomePage/AlbumCard/AlbumCard';
+import api from '../../../api/api';
 
 import './SearchResult.scss';
 
-function SearchResult({ albums }) {
+function SearchResult() {
+  const { search } = useParams();
+
+  const [albums, setAlbums] = useState([]);
+
+  console.log(search);
+  useEffect(() => {
+    api
+      .post('/albums/search', { search: search })
+      .then((res) => {
+        setAlbums(res.data);
+      // navigate('/resultat-recherche');
+      })
+      .catch((err) => {
+        console.log("Erreur, l'API ne fonctionne plus. Rechargez plus tard.");
+        console.error(err);
+      });
+  }, [search]);
   return (
     <div className="SearchResult">
+      <h2>Résultats de recherche pour "{search}"</h2>
       <div className="SearchResult-Container">
         {albums && albums.map((album) => (
           <Link to={`/albums/${album.id}`} key={album.id}>
