ea21b9fd1a293adc086c879440d1c3b8c153bbc3
sahramarchand <sahra.brooks.sb@gmail.com>
2023-06-27 12:52:14 +0200

    add 404 page and fix view header

--- Front/src/components/Header/Header.jsx
+++ Front/src/components/Header/Header.jsx
@@ -1,6 +1,3 @@
-// = Import : npm
-import { useLocation } from 'react-router-dom';
-
 // = Import : JSX
 import SearchBar from '../SearchBar/SearchBar';
 import videoBg from '../../assets/retro.mp4';
@@ -10,19 +7,6 @@ import './Header.scss';
 
 // = Header Component
 function Header() {
-// if page url contains...
-  const location = useLocation();
-  const isConnexionPage = location.pathname.startsWith('/connexion');
-  const isInscriptionPage = location.pathname.startsWith('/inscription');
-  const isAlbumPage = location.pathname.startsWith('/albums');
-  const isProfilage = location.pathname.startsWith('/profil');
-
-  // ... header is not display
-  if ((isConnexionPage || isInscriptionPage || isAlbumPage || isProfilage)) {
-    return null;
-  }
-
-  // for the other pages, header is displayed
   return (
     <div className="Header">
       <div className="Header-Overlay" />
