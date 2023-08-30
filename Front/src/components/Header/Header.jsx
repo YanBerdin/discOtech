c68b0964de34b0ab90f7c298b5a88343b3491a2b
evaredon <evaredon2001@gmail.com>
2023-08-31 00:03:18 +0200

    Change background video in image

--- Front/src/components/Header/Header.jsx
+++ Front/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 // = Import : local
 import SearchBar from '../SearchBar/SearchBar';
-import videoBg from '../../assets/background/retro.mp4';
+import synthwave from '../../assets/background/synthwave.jpg';
 
 // = Import : style
 import './Header.scss';
@@ -10,7 +10,7 @@ function Header() {
   return (
     <div className="Header">
       <div className="Header-Overlay" />
-      <video className="Header-Video" src={videoBg} autoPlay loop muted />
+      <img className="Header-Image" alt="Background Synthwave" src={synthwave} />
       <div className="Header-Content">
         <h1 className="md:text-5xl text-3xl text-white font-extrabold mb-3 md:text-center Header-Disco">
           DISCO'TECH
